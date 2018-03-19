77d5b0bb4b7ec5aafff214925ea481f816f19b75
Ian Hutchinson <ihutch@mit.edu>
2018-03-19 16:53:46 -0400

    Signs and magnitudes of the forces now consistent. There should not be exact agreement in trapped force because of the beta approximations.

--- shiftmode.f90
+++ shiftmode.f90
@@ -4,7 +4,7 @@ module shiftmode
   integer, parameter :: nx=100, ne=40, nvy=30
   real :: xL=20.,Emax=4.,vymax=4.            ! Hole length, Energy, v_y 
   real :: psi=.1,pL=4.,k=.01, Ty=1.          ! psi, sech4width, k, Ty
-  real :: beta=-5.                           ! inverse hole parallel temp.
+  real :: beta                               ! inverse hole parallel temp.
   real :: pi=3.1415926   
   complex :: omega=(0.0,.01)                  ! complex frequency
   integer :: idebug=0
@@ -12,10 +12,10 @@ module shiftmode
   ! Position arrays
   real :: dx
   real :: x(nx),phi(nx),phiprime(nx),tau(nx),v(nx)
-  real :: denad(nx),priorda(nx),denadtemp(nx)
+  real :: denad(nx),priorda(nx)
   real :: xt(nx),phit(nx),phitprime(nx)      ! Values in trapped region
   complex :: Lt(nx),phiut(nx),dent(nx)       ! tilde L,phiu,density
-  complex :: denttemp(nx),priorcontrib(nx),C(nx)
+  complex :: priorcontrib(nx),phiutest(nx)
   complex :: ft1(nx),ft2(nx),ft3(nx),ft1int(nx),ft2int(nx),ft3int(nx)
   ! Energy arrays
   real :: de,E(ne),fe0(ne),fe0de(ne)
@@ -41,7 +41,8 @@ contains
     fywy=-fy/Ty                                 ! gradient
     dtaumax=0.
     beta=-1.-(15./16.)*sqrt(pi/psi) 
-      beta=beta*(.8-.9/abs(beta))    ! Ad hoc correction.
+!      beta=beta*(.8-.9/abs(beta))    ! Ad hoc correction density
+!      beta=beta*(1.-.75/abs(beta))    ! Ad hoc correction force.
   end subroutine initialize
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  
 !Trapped particle routines. Can only be called after initialization.
@@ -251,8 +252,6 @@ contains
     complex :: omegad      ! Input omega'
     complex :: Ltint,Ltemp
     logical :: ldone=.false.
-
-
     thisdtaumax=0.
     scalefactor=1.e4       ! To avoid overflows we need intermediate scaling
     taustep=alog(scalefactor)/max(imag(omegad),1.e-5) ! tau-value at which
@@ -303,6 +302,49 @@ contains
     
   end subroutine tauxcalc
   !--------------------------------------------
+  subroutine tauxcalcnew(vinf,omegad,thisdtaumax)  
+    ! Passing particles
+    ! integrate dx/v to get v(x), tau(x), Lt(x,t=0), phiut, at vinf,omegad
+    ! return maximum dtau used for accuracy checking
+    real    :: vinf,thisdtaumax
+    complex :: omegad      ! Input omega'
+    complex :: expdtau
+    logical :: ldone=.false.
+    tau(1)=0.
+    thisdtaumax=0.
+    v(1)=sqrt(vinf**2+2.*phi(1))
+    Lt(1)=0.                                          ! Lt integrated
+    phiut(1)=v(1)-vinf
+    phiut(1)=0.                                       ! More consistent.
+! Do \int_-\infty^t exp(-i*omegad*tau) dtau, passing: Lt(i). And phiut(i)
+    do i=2,nx
+       v(i)=sqrt(vinf**2+2.*phi(i))
+       dtau=dx*0.5*(v(i-1)+v(i))/(v(i-1)*v(i))        ! tau increment varies
+       if(dtau.gt.thisdtaumax)thisdtaumax=dtau
+       tau(i)=tau(i-1)+dtau
+       expdtau=exp(sqm1*omegad*dtau)
+       vmean=(v(i)+v(i-1))/2.-vinf
+       Lt(i)=expdtau*Lt(i-1)-vmean*(1.-expdtau)/(omegad)   ! New integral
+       !       phiut(i)=omegad*Lt(i)*exp(sqm1*omegad*tau(i))
+       phiut(i)=omegad*Lt(i)
+    enddo
+    if(idebug.gt.0.and..not.ldone)then
+       write(*,'(a,2f8.4,a,f8.4,a,f8.2)')'omegad',omegad,' vinf', &
+         vinf,' taustep',taustep
+       write(*,*)'phi'
+       write(*,'(10f8.4)')phi
+       write(*,*)'v'
+       write(*,'(10f8.4)')v
+       write(*,*)'tau'
+       write(*,'(10f8.2)')tau
+       write(*,*)'real(Lt)'
+       write(*,'(10f8.1)')real(Lt)
+       write(*,*)'real(phiut)'
+       write(*,'(10f8.2)')real(phiut)
+       ldone=.true.
+    endif
+  end subroutine tauxcalcnew
+  !--------------------------------------------
   subroutine ftildecalc(vinf,fe,dfe)   ! Integrate over vy to get ft-parallel
                    ! fe is parallel distrib, dfe is its derivative wrt E.
     ft1int=0. ; ft2int=0. ; ft3int=0.
@@ -312,11 +354,11 @@ contains
     ! all the tauxcalcs at once on the array.
     do i=1,nvy
        omegad=omega-k*vy(i)
-       call tauxcalc(vinf,omegad,dtaumax) ! Integrate along past orbit
+       call tauxcalcnew(vinf,omegad,dtaumax) ! Integrate along past orbit
                                           ! to get phiut, hence ft1-3
        ft1=dfe*fy(i)*phiut
        ft2=dfe*fy(i)*vy(i)*phiut
-       ft3=fe*fy(i)*vy(i)*phiut
+       ft3=fe*fywy(i)*vy(i)*phiut
        ft1int=ft1int+ft1*dvy
        ft2int=ft2int+ft2*dvy
        ft3int=ft3int+ft3*dvy
@@ -329,6 +371,7 @@ contains
     ! On the way, calculate the f(x,E) array. Only non-adiabatic.
     ! Also calculate the adiabatic density perturbation, denad.
     ! So far only positive velocity direction. 
+    sq2pi=sqrt(2.*pi)
     vinfmax=sqrt(2.*Emax)      ! The uppermost orbit
     dvinf=vinfmax/ne           ! Use steps of constant vinf spacing.
     denad=0.                   ! adiabatic density 
@@ -337,11 +380,12 @@ contains
     do i=1,ne                  ! Integrate over ne orbits (energies)
        vinf=(i-0.5)*dvinf
        E(i)=vinf**2/2.
-       fe0(i)=exp(-E(i)) ! Unit height Maxwellian with unit temperature.
-       fe0de(i)=fe0(i)   ! Derivative wrt E is same.
+       fe0(i)=exp(-E(i))/sq2pi ! Normalized Maxwellian with unit temperature.
+       fe0de(i)=-fe0(i)   ! Derivative wrt E is minus the same.
        ! ftildecalc calls tauxcalc which also calculates v(nx) for this vinf.
        call ftildecalc(vinf,fe0(i),fe0de(i))
-       fte(:,i)=(omega*ft1int+k*(-ft2int+ft3int))*sqm1  ! tilde-f_x(x,E)
+       ! No factor q_e has been applied to phiut, so no q_e factor here.
+       fte(:,i)=sqm1*(omega*ft1int+k*(-ft2int+ft3int))  ! tilde-f_x(x,E)
        if(idebug.gt.1)write(*,'(i4,10f8.4)')i,E(i),vinf,fe0(i),omega,k
        if(i.eq.1.and.idebug.gt.1)then
           write(*,*)'  i    E(i)   vinf   fe0(i)  omegar  omegai    k'
@@ -356,13 +400,15 @@ contains
           write(*,*)'phiut'
           write(*,'(10f8.4)')phiut
        endif
-       denttemp=fte(:,i)*vinf/v  
-       dent=dent+denttemp*dvinf  ! Non-adiabatic
+       dent=dent+(fte(:,i)*vinf/v)*dvinf  ! Non-adiabatic
 ! (-dphi/dx=\hat\phi)*(df/dE dv) for \Delta=1. ! Adiabatic not needed.
-       denadtemp=-phiprime*fe0de(i)*vinf/v
-       denad=denad+denadtemp*dvinf 
+!       denad=denad-phiprime*fe0de(i)*vinf/v*dvinf 
 ! end of adiabatic section.
     enddo
+! For stationary holes negative velocities just double the effective ne
+! (Although actually it is the force they double and non-zero hole speed
+! would require this to be done differently.)
+    dent=dent*2
   end subroutine dentcalc2
 !  include 'obsoletecode.f90'
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -372,16 +418,17 @@ include 'fhgfunc.f'
 
 program main
   use shiftmode 
-  integer, parameter ::   nk=21
+  integer, parameter ::   nk=2
   real :: kik(nk)
   complex :: Fcpassing(nk),Ftrapped(nk),Ftotal
   integer, parameter :: np=50
 !  integer, parameter :: np=2
-  real :: psinp(np),Ftnp(np),Fpnp(np),hnp(np),gnp(np),hmgnp(np)
-
-  omega=(0.0,.05)
+  real :: psinp(np),Ftnp(np),Fpnp(np),hnp(np),gnp(np),hmgnp(np),gjnp(np),pnp(np)
+  omega=(0.0,.01)
+  psistep=.01
+  call pfset(3)
   do ip=1,np
-     psi=.01*ip
+     psi=psistep*ip
 !     psi=.1*ip
      psinp(ip)=psi
   call initialize
@@ -400,14 +447,15 @@ program main
 
   idebug=0
 ! k-scan
-  akmax=.04
+  akmax=.008
   write(*,*)'   k     Fpassing                Ftrapped'
-  do ik=1,1
+  do ik=1,2
      k=(ik-1.)*akmax/(nk-1.)
      kik(ik)=k
      call dentcalc2
 !     write(*,*)'dtaumax=',dtaumax,' kvymax.dtaumax=',k*vymax*dtaumax
-! Integrate phi'*n-tilde dx     
+! Integrate phi'*n-tilde dx.  
+! Should we double the positive ntilde contribution to account for negative?
      Fcpassing(ik)=0.
      do i=1,nx
         Fcpassing(ik)=Fcpassing(ik)+phiprime(i)*dent(i)*dx
@@ -417,21 +465,23 @@ program main
      write(*,'(f6.4,4es12.3)')k,Fcpassing(ik),Ftraptotal
   enddo
 
-  call fhgfunc(psi,20.,100,pL,gave,have,gjint,pint)
+  call fhgfunc(psi,20.,100,pL,gave,have,gjave,pint)
   so=-imag(omega)**2
-  write(*,*)'  psi      ( have=Total    gave=Ft     have-gave)*(-omega_i^2)'
-  write(*,'(5es12.4)')psi,have*so,gave*so &
-       ,(have-gave)*so,so
+!  write(*,*)'  psi      ( have=Total    gave=Ft     have-gave)*(-omega_i^2)'
+!  write(*,'(5es12.4)')psi,have*so,gave*so &
+!       ,(have-gave)*so,so
   
   Ftnp(ip)=real(Ftraptotal)/so
   Fpnp(ip)=real(Fcpassing(ik-1))/so
   hnp(ip)=have
   gnp(ip)=gave
-  hmgnp(ip)=-(have-gave)
+  gjnp(ip)=gjave
+  pnp(ip)=-pint
+  hmgnp(ip)=(have-gave)
   enddo
 
   if(np.gt.1)then
-     call pltinit(0.,.5,0.,2.)
+     call pltinit(0.,np*psistep,-np*psistep*1.3,np*psistep*4.)
      call axis
      call axlabels('psi','normalized force')
      call polyline(psinp,Ftnp,np)
@@ -439,9 +489,12 @@ program main
      call polyline(psinp,Fpnp,np)
      call jdrwstr(wx2nx(psinp(np/2)),wy2ny(Fpnp(np/2)),'Passing',-1.)
      call color(3)
-!     call polyline(psinp,hnp,np)
+     call dashset(1)
      call polyline(psinp,gnp,np)
      call polyline(psinp,hmgnp,np)
+!     call polyline(psinp,gjnp,np) ! trapped force w/o boundary term
+!     call polyline(psinp,pnp,np)  ! passing force w/o boundary term
+     call dashset(0)
      call pltend()
   endif
 
